661131c8c217ba9bb1ac0fd75d31908b9b421f37
Tina Dumilina <tina.dumilina@gmail.com>
2020-05-12 16:39:02 +0300

    form added

--- components/ui/Input.vue
+++ components/ui/Input.vue
@@ -4,7 +4,17 @@
 
 <style scoped>
 .input {
+  padding: 10px;
   font-size: 18px;
   line-height: 24px;
+}
+
+.input::placeholder {
+  font-family: Inter;
+  color: #666;
+}
+
+.input:focus {
+  outline: 1px solid #613a93;
 }
 </style>
