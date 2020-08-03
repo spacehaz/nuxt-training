3cfc2d26973b6b92115b56e698ef8950dcd8caa2
batvale1 <batvale1@yandex.ru>
2020-08-04 00:45:42 +0300

    tags endpoing, flex, custom input sections

--- pages/stories/_id.vue
+++ pages/stories/_id.vue
@@ -121,7 +121,7 @@ export default {
       const date = new Date(value);
       return date.toLocaleString('ru', {
         year: 'numeric',
-        month: 'long',
+        month: 'short',
         day: 'numeric',
       });
     },
