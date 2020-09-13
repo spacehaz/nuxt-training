fa5b874f0d4bc8383483724ef8c94cd6628e3502
batvale1 <batvale1@yandex.ru>
2020-09-13 10:20:52 +0300

    seo scripts are added

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
+    __dangerouslyDisableSanitizers: ['script'],
     title: process.env.npm_package_name || '',
     htmlAttrs: {
       lang: 'ru',
@@ -18,6 +19,43 @@ export default {
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    noscript: [
+      {
+        innerHTML: `<div><img src="https://mc.yandex.ru/watch/56164720" style="position:absolute; left:-9999px;" alt="" /></div>`,
+      },
+    ],
+    script: [
+      {
+        hid: 'gtm-script1',
+        innerHTML: `
+           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+           ym(56164720, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+           });
+        `,
+        type: 'text/javascript',
+        charset: 'utf-8',
+        defer: true,
+      },
+      {
+        hid: 'gtm-script2',
+        innerHTML: `
+           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-88049847-1', 'auto');
+            ga('send', 'pageview');
+        `,
+        type: 'text/javascript',
+        charset: 'utf-8',
+        defer: true,
+      },
+    ],
   },
   /*
    ** Customize the progress-bar color
